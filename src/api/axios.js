9fecc7851796a0e1ad0241665a653383a3848028
hm32295 <almkhbz245@example.com>
2026-08-09 18:19:08 +0300

    dashboard

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: import.meta.env.VITE_API_URL || 'https://ecommerce-shark-git-main-hm32295-6224s-projects.vercel.app/api/v1',
   withCredentials: true,
 });
 
